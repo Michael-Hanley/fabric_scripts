12cabbc6fc57bcb9703a0e6dd089143c62a31fe8
Pasquali <Pasaquali@gmail.com>
2015-11-13 09:14:27 -0800

    This is my first fabric script, It works for what I needed to do but I am unsure if my general practices are correct. It currently runs well on a local ubuntu install.

---
+++ fabfile-django_install.py
@@ -0,0 +1,40 @@
+from __future__ import with_statement
+from fabric.api import *
+from contextlib import contextmanager as _contextmanager
+from fabric.context_managers import lcd
+
+def install():
+    execute(python)
+    execute(pip)
+    execute(django)
+    execute(virt_env)
+    execute(apache)
+    execute(setup_tools)
+    execute(create_virtual)
+    execute(start_django_proj)
+
+def python():
+    local("sudo apt-get install python")
+
+def pip():
+    local("sudo apt-get install python-pip python-dev build-essential ")
+
+def django():
+    local("sudo pip install Django")
+
+def virt_env():
+    local("sudo pip install virtualenv")
+
+def apache():
+    local("sudo apt-get install apache2")
+
+def setup_tools():
+    local("sudo apt-get install python-setuptools")
+
+def create_virtual():
+    local("virtualenv project")
+
+def start_django_proj():
+    code_dir = 'project'
+    with lcd(code_dir):
+        local("django-admin.py startproject myproject")
